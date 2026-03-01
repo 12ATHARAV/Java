53cc922a924906919e0ac547cd36ff0c2045045c
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-01 09:38:26 +0530

    update

---
+++ Java/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args){
+        String name = "Atharav N. Dhumone";
+        char grade = 'A';
+        int age = 20;
+        float cgpa = 9.8f;
+        boolean powerful = true;
+        System.out.println(name+grade+cgpa+age+powerful);
+
+    }
+}
