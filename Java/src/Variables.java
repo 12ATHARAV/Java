c9bd3e6adb5664c62ffc4f62ad73e0117979a1c4
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-01 09:46:43 +0530

    update

--- Java/src/Variables.java
+++ Java/src/Variables.java
@@ -7,5 +7,15 @@ public class Variables {
         boolean powerful = true;
         System.out.println(name+grade+cgpa+age+powerful);
 
+
+        int temp = 12;
+        temp = 15;
+        System.out.println(temp);
+
+
+        final int permanent = 23;
+        // permanent = 94;   this will give error
+        System.out.println(permanent);
+
     }
 }
