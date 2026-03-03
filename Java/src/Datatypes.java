26de174b59c4547b3caecb1b7345b707a4051206
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-04 04:36:18 +0530

    update

--- Java/src/Datatypes.java
+++ Java/src/Datatypes.java
@@ -1,42 +1,58 @@
-public class Datatypes {
+// public class Datatypes {
+//     public static void main(String[] args){
+//         // Part 1: Primitive data types
+//         int age = 21;
+//         System.out.println(age);
+
+//         long population = 13000000000L;
+//         System.out.println(population);
+
+//         float price = 99.34f;
+//         System.out.println(price);
+
+//         double pi = 3.14159;
+//         System.out.println(pi);
+
+//         char grade = 'A';
+//         System.out.println(grade);
+
+//         char ch = 65;            //Because 65 is Unicode for 'A'.
+//         System.out.println(ch); 
+
+//         boolean isPass = true;
+//         System.out.println(isPass);
+
+//         byte smallNumber = 100;
+//         System.out.println(smallNumber);
+
+//         short num = 30000;
+//         System.out.println(num);
+
+//         // type casting
+//         // 1. implicit casting (widening)
+//         int a = 10;
+//         double b = a;
+//         System.out.println(b);
+
+//         // 2. explicit casting (narrowing)
+//         double x = 10.99;
+//         int y = (int) x;
+//         System.out.println(y);
+
+//     }
+// }
+
+
+
+// default values
+public class Datatypes{
+    static int x;
+    static boolean y;
+
     public static void main(String[] args){
-        // Part 1: Primitive data types
-        int age = 21;
-        System.out.println(age);
-
-        long population = 13000000000L;
-        System.out.println(population);
-
-        float price = 99.34f;
-        System.out.println(price);
-
-        double pi = 3.14159;
-        System.out.println(pi);
-
-        char grade = 'A';
-        System.out.println(grade);
-
-        char ch = 65;            //Because 65 is Unicode for 'A'.
-        System.out.println(ch); 
-
-        boolean isPass = true;
-        System.out.println(isPass);
-
-        byte smallNumber = 100;
-        System.out.println(smallNumber);
-
-        short num = 30000;
-        System.out.println(num);
-
-        // type casting
-        // 1. implicit casting (widening)
-        int a = 10;
-        double b = a;
-        System.out.println(b);
-
-        // 2. explicit casting (narrowing)
-        double x = 10.99;
-        int y = (int) x;
+        System.out.println(x);
         System.out.println(y);
     }
 }
+
+
