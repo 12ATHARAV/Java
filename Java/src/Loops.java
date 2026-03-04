f286c16299ea9c1973aa4ca5e8747c1a7040f72b
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-04 16:38:39 +0530

    update

--- Java/src/Loops.java
+++ Java/src/Loops.java
@@ -44,10 +44,65 @@ public class Loops {
 
 
         // continue
-        for(int i=1; i<=5; i++){
-            if(i == 3)
-                continue;
-            System.out.println(i);
-        }
+        // for(int i=1; i<=5; i++){
+        //     if(i == 3)
+        //         continue;
+        //     System.out.println(i);
+        // }
+
+
+        // Nested Loops
+        // for(int i=1; i<=3; i++){
+        //     for(int j=1; j<=2; j++){
+        //         System.out.println("i = " + i + ", j = " + j);
+        //     }
+        // }
+
+
+        // square pattern
+        // for(int i=1; i <= 4; i++){
+        //     for(int j=1; j <= 4; j++){
+        //         System.out.print("*  ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // triangle pattern
+        // for(int i = 1; i <= 4; i++){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*  ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // tricky question  //It prints only once. Why? Because semicolon ends the loop.
+        // for(int i=0; i<5; i++);
+        // {
+        //     System.out.println("Hello");
+        // }
+
+
+        // Q1
+        // for(int i=1; i<=3; i++){
+        //     System.out.print(i + " ");
+        // }
+
+
+        // Q2
+        // int i = 1;
+        // while(i <= 3){
+        //     System.out.print(i + " ");
+        //     i++;
+        // }
+
+
+        // Q3
+        // for(int i=1; i <= 3; i++){
+        //     for(int j = 1; j <= 2; j++){
+        //         System.out.print("* ");
+        //     }
+        // }
     }
 }
