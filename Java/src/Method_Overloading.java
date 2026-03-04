5a5021d2f979c99150c9308c1e432a494659f436
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-05 04:38:55 +0530

    update

---
+++ Java/src/Method_Overloading.java
@@ -0,0 +1,17 @@
+public class Method_Overloading {
+    static int add(int a,int b){
+        return a + b;
+    }
+    static double add(double a, double b){
+        return a + b;
+    }
+    static int add(int a, int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args){
+        System.out.println(add(5,3));
+        System.out.println(add(5.1,3.9));
+        System.out.println(add(5,3,4));
+    }
+
+}
