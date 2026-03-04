382d2faa47dfa27ec3c62c2064c4caad10e9395c
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-04 16:19:43 +0530

    update

--- Java/src/Loops.java
+++ Java/src/Loops.java
@@ -28,10 +28,26 @@ public class Loops {
 
 
         // do - while
-        int i = 10;
-        do{
+        // int i = 10;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i < 5);
+
+
+        // break
+        // for(int i=1; i<=10; i++){
+        //     if(i == 5)
+        //         break;
+        //     System.out.println(i);
+        // }
+
+
+        // continue
+        for(int i=1; i<=5; i++){
+            if(i == 3)
+                continue;
             System.out.println(i);
-            i++;
-        }while(i < 5);
+        }
     }
 }
