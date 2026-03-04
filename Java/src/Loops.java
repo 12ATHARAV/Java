886982dcc0525594fd710688223edb70e0291e3c
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-04 16:14:47 +0530

    update

---
+++ Java/src/Loops.java
@@ -0,0 +1,37 @@
+public class Loops {
+    public static void main(String[] args){
+        // 1. for Loop
+        // print 1 to 5
+        // for(int i = 1; i <= 5; i++){
+        //     System.out.println(i);
+        // }
+
+
+        //reverse loop
+        // for(int i = 5; i >= 1; i--){
+        //     System.out.println(i);
+        // }
+
+
+        // infinite loop (condition missing)
+        // for(;;){
+        //     System.out.println("Hello");
+        // }
+
+    
+        // while loop
+        // int i = 1;
+        // while(i <= 5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        // do - while
+        int i = 10;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i < 5);
+    }
+}
