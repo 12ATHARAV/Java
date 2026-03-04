374f6e34c10f93fdac811c252e533969944e28da
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-05 04:41:49 +0530

    update

--- Java/src/Method_Overloading.java
+++ Java/src/Method_Overloading.java
@@ -1,17 +1,30 @@
 public class Method_Overloading {
-    static int add(int a,int b){
-        return a + b;
+    // static int add(int a,int b){
+    //     return a + b;
+    // }
+    // static double add(double a, double b){
+    //     return a + b;
+    // }
+    // static int add(int a, int b, int c){
+    //     return a + b + c;
+    // }
+    // public static void main(String[] args){
+    //     System.out.println(add(5,3));
+    //     System.out.println(add(5.1,3.945));
+    //     System.out.println(add(5,3,4));
+    // }
+
+
+    // E2
+    static void greet(){
+        System.out.println("Hello");
     }
-    static double add(double a, double b){
-        return a + b;
-    }
-    static int add(int a, int b, int c){
-        return a + b + c;
+    static void greet(String name){
+        System.out.println("Hello, " + name);
     }
     public static void main(String[] args){
-        System.out.println(add(5,3));
-        System.out.println(add(5.1,3.9));
-        System.out.println(add(5,3,4));
+        greet();
+        greet("Atharav");
     }
 
 }
