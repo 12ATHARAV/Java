8bd9ec86259c93b39845d20718e0f5ddd31e7ec9
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-04 17:00:02 +0530

    update

---
+++ Java/src/Methods.java
@@ -0,0 +1,39 @@
+public class Methods {
+    // simple method //
+    // static void greet(){
+    //     System.out.println("Hello Atharav");
+    // }
+    // public static void main(String[] args){
+    //     greet(); //calling the method
+    // }
+
+
+    // method with parameters //
+    // static void greet(String name){
+    //     System.out.println("Hello " + name);
+    // }
+    // public static void main(String[] args){
+    //     greet("Atharav");
+    //     greet("Sanjana");
+    // }
+
+
+    // Method with Return Value //
+    // static int add(int a, int b){
+    //     return a + b;
+    // }
+    // public static void main(String[] args){
+    //     int result = add(6,8);
+    //     System.out.println(result);
+    // }
+
+
+    // Q1
+    static int square(int x){
+        return x * x;
+    }
+    public static void main(String[] args){
+        int result = square(4);
+        System.out.println(result);
+    }
+}
