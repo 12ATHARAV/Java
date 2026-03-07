4a03e897373462ae0741c8dad643c6770d8aa103
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-07 15:54:47 +0530

    update

---
+++ Java/src/String.java
@@ -0,0 +1,9 @@
+public class String {
+    public static void main(String[] args){
+        ///////// string creation ways ////////
+        /// method 1 ---> string pool
+        String s = "Atharav";
+
+        
+    }
+}
