e48ff16fdc17ba39778340ef7be94affbcb8237c
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-08 11:52:10 +0530

    update

--- Java/src/OOPS/Constructor.java
+++ Java/src/OOPS/Constructor.java
@@ -42,3 +42,23 @@ package OOPS;
 
 
 ////////  Parameterized Constructor  ////////
+
+class Student{
+    int roll;
+    String name;
+
+    Student(int r, String n){
+        roll = r;
+        name = n;
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args){
+        Student s1 = new Student(1, "Atharav");
+        Student s2 = new Student(2, "Jeff");
+
+        System.out.println(s1.name);
+        System.out.println(s2.roll);
+    }
+}
