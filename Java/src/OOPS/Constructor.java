b3ff1304596a01e79d6b0facc7dced098116ba87
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-08 11:46:41 +0530

    update

--- Java/src/OOPS/Constructor.java
+++ Java/src/OOPS/Constructor.java
@@ -3,14 +3,42 @@ package OOPS;
 
 ///////  Default Constructor  ///////
 
-class Student{
-    Student(){
-        System.out.println("Constructor called");
-    }
-}
+// class Student{
+//     Student(){
+//         System.out.println("Constructor called");
+//     }
+// }
 
-public class Constructor {
-    public static void main(String[] args){
-        Student s1 = new Student();
-    }
-}
+// public class Constructor {
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+//     }
+// }
+
+
+
+
+/////// Constructor for Initialization //////
+
+// class Student{
+//     int roll;
+//     String name;
+
+//     Student(){
+//         roll = 1;
+//         name = "Atharav";
+//     }
+// }
+
+// public class Constructor {
+//     public static void main(String[] args){
+//         Student s1 = new Student();
+
+//         System.out.println(s1.roll);
+//         System.out.println(s1.name);
+//     }
+// }
+
+
+
+////////  Parameterized Constructor  ////////
