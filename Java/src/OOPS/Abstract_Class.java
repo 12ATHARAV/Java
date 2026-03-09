077e2d04ce07c97f9ee42670e177d9e39c83ea89
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-09 20:49:36 +0530

    update

---
+++ Java/src/OOPS/Abstract_Class.java
@@ -0,0 +1,97 @@
+package OOPS;
+
+// abstract class Animal{
+//     abstract void sound();
+// }
+
+// class Dog extends Animal{
+//     void sound(){
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// class Cat extends Animal{
+//     void sound(){
+//         System.out.println("Cat meows");
+//     }
+// }
+
+// public class Abstract_Class
+// {
+//     public static void main(String[] args){
+//         Animal a;
+
+//         a = new Dog();
+//         a.sound();
+
+//         a = new Cat();
+//         a.sound();
+//     }
+// }
+
+
+
+
+// 6️⃣ Example with Normal + Abstract Method
+// abstract class Animal
+// {
+//     void eat()
+//     {
+//         System.out.println("Animals eat food");
+//     }
+
+//     abstract void sound();
+// }
+
+// class Dog extends Animal
+// {
+//     void sound()
+//     {
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// public class Abstract_Class
+// {
+//     public static void main(String[] args)
+//     {
+//         Dog d = new Dog();
+
+//         d.eat();
+//         d.sound();
+//     }
+// }
+
+
+
+
+//////// Complete Example (Important) //////// 
+
+// abstract class Animal{
+//     abstract void sound();
+// }
+// class Dog extends Animal{
+//     void sound(){
+//         System.out.println("Dog Barks");
+//     }
+// }
+// class Cat extends Animal{
+//     void sound(){
+//         System.out.println("Cat meows");
+//     }
+// }
+// public class Abstract_Class {
+//     public static void main(String[] args){
+//         Animal a;
+
+//         a = new Dog();
+//         a.sound();
+
+//         a = new Cat();
+//         a.sound();
+//     }
+// }
+// Animal → abstract class
+// Dog, Cat → concrete classes
+
+
---
+++ Java/src/primeNo.java
@@ -0,0 +1,33 @@
+// import java.util.Scanner;
+
+// class primeLogic {
+//     public boolean isPrime(int n) {
+//         if (n <= 1) return false;
+        
+//         for(int i=2; i <= Math.sqrt(n); i++){
+//             if(n % i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+// public class primeNo {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a integer: ");
+//         int num = sc.nextInt();
+
+//         primeLogic pl = new primeLogic();
+
+//         if(pl.isPrime(num)){
+//             System.out.println(pl.isPrime(num));
+//         }else{
+//             System.out.println(pl.isPrime(num));
+//         }
+//     }
+// }
+
+
+
+//write a method which accepts 2 nos and prints all the prime nos. between those
