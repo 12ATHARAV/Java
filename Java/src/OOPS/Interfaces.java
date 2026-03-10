0b4b72014a293b6a7bcab3817687c551c1f0b05c
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-10 07:19:03 +0530

    update

---
+++ Java/src/OOPS/Interfaces.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+// 1️⃣ What is an Interface?
+// An interface is a blueprint of a class that contains abstract methods.
+// It defines what a class should do, not how it does it.
+
+// 2️⃣ Syntax
+// interface Animal
+// {
+//     void sound();
+// }
+
+// Methods are abstract by default.
+
+
+
+/////////// 3️⃣ Complete Example ////////////
+
