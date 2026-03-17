ba2c6513c2a62c0f8532dca15153f0ac23c34e6a
Atharav Dhumone <atharavdhumone101@gmail.com>
2026-03-17 21:32:08 +0530

    update

--- Questions/src/Strings/RemoveDuplicates.java
+++ Questions/src/Strings/RemoveDuplicates.java
@@ -1,5 +1,26 @@
 package Strings;
 
+// Approach
+// Use HashSet.
+
+import java.util.*;
+
 public class RemoveDuplicates {
-    
+    public static String removeDuplicates(String s){
+        HashSet<Character> set = new HashSet<>();
+
+        String result = "";
+
+        for(char c: s.toCharArray()){
+            if(!set.contains(c)){
+                set.add(c);
+                result += c;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("programming"));
+    }
 }
